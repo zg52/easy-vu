93103c5bc948c5b19581d60fd40e700dc40568d0
zg <lonrui@163.com>
2019-10-15 16:35:09 +0800

    启用登录

--- lonarui/src/mock/mock.js
+++ lonarui/src/mock/mock.js
@@ -1,18 +1,34 @@
 // 使用 Mock
 var Mock = require('mockjs')
-var datas = Mock.mock('http://www.zg.com',{
-  "login": [
-     {
-        "name": "longarui",
-        "password": "4321"
-     }
-  ],
-  "msg": "登录成功",
-  "success": true,
-  "statusCode": 200,
-  "number|+1": 202,
-  "vip|1-8": "★"
+var datas = Mock.mock('http://www.zg.com', {
+   "login": [
+      {
+         "id": 1,
+         "name": "月弦笙音",
+         "password": "4321"
+      },
+      {
+         "id": 2,
+         "name": "倩倩",
+         "password": "4321"
+      },
+      {
+         "id": 3,
+         "name": "纳尼",
+         "password": "4321"
+      },
+      {
+         "id": 4,
+         "name": "好的呢",
+         "password": "4321"
+      }
+   ],
+   "msg": "登录成功",
+   "success": true,
+   "statusCode": 200,
+   "error": 500,
+   "number|+1": 202,
+   "vip|1-8": "★"
 })
 // 输出结果
- 
- 
+
