1a5e05d07cac51107948ff999e940170e37d3e68
zg <lonrui@163.com>
2019-11-21 18:03:11 +0800

    配置render 函数

--- lonarui/src/components/crm/index.vue
+++ lonarui/src/components/crm/index.vue
@@ -3,13 +3,17 @@
 <template>
   <div>
     <router-link to="./listDetails">用户列表</router-link>
- <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+ <!-- <div id="myChart" :style="{width: '300px', height: '300px'}"></div> -->
+  <op :level="1">this is render
+  <div>jij</div>
+  </op>
   </div>
 </template>
 <script>
- 
+ import op from './op.vue'
 export default {
   components:{
+    op
   },
   data() {
     return {
@@ -19,28 +23,29 @@ export default {
   methods: {
     drawLine() {
       // 基于准备好的dom，初始化echarts实例
-      let myChart = this.$echarts.init(document.getElementById("myChart"));
-      // 绘制图表
-myChart.setOption({
-    series : [
-        {
-            name: '访问来源',
-            type: 'pie',
-            radius: '55%',
-            data:[
-                {value:235, name:'视频广告'},
-                {value:274, name:'联盟广告'},
-                {value:310, name:'邮件营销'},
-                {value:335, name:'直接访问'},
-                {value:400, name:'搜索引擎'}
-            ]
-        }
-    ]
-})
+//       let myChart = this.$echarts.init(document.getElementById("myChart"));
+//       // 绘制图表
+// myChart.setOption({
+//     series : [
+//         {
+//             name: '访问来源',
+//             type: 'pie',
+//             radius: '55%',
+//             data:[
+//                 {value:235, name:'视频广告'},
+//                 {value:274, name:'联盟广告'},
+//                 {value:310, name:'邮件营销'},
+//                 {value:335, name:'直接访问'},
+//                 {value:400, name:'搜索引擎'}
+//             ]
+//         }
+//     ]
+// })
     }
   },
   mounted() {
     this.drawLine();
+    console.log(this.$attrs )
   }
 };
 </script>
---
+++ lonarui/src/components/crm/op.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: {
+    level: {
+      type: Number,
+      required: true
+    }
+  },
+  render (ce) {
+    return ce(
+      "div", // 标签名称
+      this.$slots.default // 子节点数组
+    );
+  }
+};
+</script>
